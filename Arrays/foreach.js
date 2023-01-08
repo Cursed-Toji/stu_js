f95eba99518e9630535d28a9170cf2d10cb6ca97
Cals <88802566+victorcals@users.noreply.github.com>
2023-01-08 09:51:01 -0300

    JS Att

--- Arrays/foreach.js
+++ Arrays/foreach.js
@@ -1,7 +1,11 @@
-const aprovados = ['agatha', 'aide', 'bia', 'nicole']
-
+const aprovados = ['agatha', 'aldo', 'daniel', 'rafaela']
 aprovados.forEach(function (nome, indice) {
     console.log(`${indice + 1}) ${nome}`)
+
 })
 
-console.log(aprovados)
+aprovados.forEach(nome => console.log(nome))
+
+
+const exibir = aprovados => console.log(aprovados)
+aprovados.forEach(exibir)
