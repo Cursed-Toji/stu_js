59aac99dd676ac527bee7b9345edd5a5077fcbe4
victorcals <victorcals78@gmail.com>
2023-01-20 15:34:46 -0300

    20/01 att

--- node/UsandoModuloTerceiro.js
+++ node/UsandoModuloTerceiro.js
@@ -1,3 +1,7 @@
 const _ = require('lodash')
 
 setInterval(() => console.log(_.random(1, 1000)), 2000)
+
+
+// abrir o power shell como adm
+// https://pt.stackoverflow.com/questions/220078/o-que-significa-o-erro-execução-de-scripts-foi-desabilitada-neste-sistema
