b5e3f04a4bff58ef78b5f114239a352f855916ff
victorcals <victorcals78@gmail.com>
2023-02-28 17:47:57 -0300

    28/02

---
+++ node/temporizador.js
@@ -0,0 +1,21 @@
+const schedule = require('node-schedule')
+
+const tarefa1 = schedule.scheduleJob('*/5 * 09 * * 2', function () {
+    console.log('exetucanto tarefa 1', new Date().getSeconds())
+})
+
+
+setTimeout(function () {
+    tarefa1.cancel()
+    console.log('cancelamneto tarefa 1')
+}, 20000)
+
+
+const regra = new schedule.RecurrenceRule()
+regra.dayOfWeek = [new schedule.Range(1, 5)]
+regra.hour = 09
+regra.second = 30
+
+const tarefa2 = schedule.scheduleJob(regra, function () {
+    console.log('exetucanto tarefa 2', new Date().getSeconds())
+})
