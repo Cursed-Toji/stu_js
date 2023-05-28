baa7f2f5444e1f2cdebd8ac40a0138f7fdfc88ab
Cals <88802566+victorcals@users.noreply.github.com>
2023-05-28 15:55:42 -0300

    Controller

---
+++ Esnext/map.js
@@ -0,0 +1,5 @@
+const tecnologia = new Map()
+tecnologia.set('react', { framework: false })
+tecnologia.set('angular', { framework: true })
+console.log(tecnologia.react)
+console.log(tecnologia.get('react').framework)
---
+++ Esnext/taggedTemplate1.js
@@ -0,0 +1,12 @@
+// tagged templates - processsa o template dentro de uma função 
+
+function tag(partes, ...valores) {
+    console.log(partes)
+    console.log(valores)
+    return 'outra string'
+}
+
+const aluno = 'gui'
+const situacao = 'aprovado'
+
+console.log(tag` ${aluno} está ${situacao}`)
